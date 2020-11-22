1978315dcfb3f8a3ce233ac0afbf8e084d03fa4c
Curder <q.curder@gmail.com>
2020-11-22 18:12:31 +0800

    code style fixed

--- 7.0/scalar_typehints.php
+++ 7.0/scalar_typehints.php
@@ -1,4 +1,6 @@
 <?php
+// 参数类型的声明
+
 declare(strict_types = 1);
 
 function setAge(int $age)
@@ -13,9 +15,9 @@ function setIsValid(bool $valid)
 
 // 默认会进行自动类型转换
 setAge(29); // int(29)
-// setAge('29'); // 默认不定义`strict_types =1 ` 会返回 int(29) ；如果定义则会抛出错误
+// setAge('29'); // 默认不定义`strict_types=1` 会返回 int(29) ；如果定义则会抛出错误
 
 // 如果不能进行类型转换则会抛出错误
 // setAge('abs'); // 抛出错误
-// setIsValid('abs'); // 默认不定义`strict_types =1 ` 会返回 bool(true) ；如果定义则会抛出错误
+// setIsValid('abs'); // 默认不定义`strict_types=1` 会返回 bool(true) ；如果定义则会抛出错误
 // setIsValid([]); // 抛出错误
