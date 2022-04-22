f76fce192739cfd03859ee578423c40101496c29
curder <curder@users.noreply.github.com>
2022-04-22 02:14:33 +0000

    deploy: 4d4e250c9c3e9ddf077076f5e3e4fc8e3a0a6976

---
+++ assets/index.html.490ed81c.js
@@ -0,0 +1,11 @@
+import{_ as o,r as t,o as r,c as i,w as e,a as l,b as n,d as s}from"./app.27c7bfba.js";const u={},p=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token comment"},"# install in your project"),s(`
+
+`),n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token function"},"watch"),s(),n("span",{class:"token comment"},"# start writing"),s(`
+
+`),n("span",{class:"token function"},"yarn"),s(" prod "),n("span",{class:"token comment"},"# build to static files"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br")])],-1),m=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"npm"),s(),n("span",{class:"token function"},"install"),s(),n("span",{class:"token comment"},"# install in your project"),s(`
+
+npx `),n("span",{class:"token function"},"watch"),s(),n("span",{class:"token comment"},"# start writing"),s(`
+
+npx prod `),n("span",{class:"token comment"},"# build to static files"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br")])],-1);function d(b,_){const a=t("CodeGroupItem"),c=t("CodeGroup");return r(),i(c,null,{default:e(()=>[l(a,{title:"YARN",active:""},{default:e(()=>[p]),_:1}),l(a,{title:"NPM"},{default:e(()=>[m]),_:1})]),_:1})}var h=o(u,[["render",d],["__file","index.html.vue"]]);export{h as default};
