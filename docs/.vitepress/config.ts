3f6326974704551d1b39269f47ba3845f3ee18ad
curder <q.curder@gmail.com>
2024-05-16 11:37:36 +0800

    Change php version sort

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -34,27 +34,28 @@ export default defineConfig({
         },
         nav: [
             {text: '首页', link: '/'},
-            {
-                text: 'PHP 7',
-                activeMatch: '/7',
-                items: [
-                    {text: '7.0', link: '/7.0/'},
-                    {text: '7.1', link: '/7.1/'},
-                    {text: '7.2', link: '/7.2/'},
-                    {text: '7.3', link: '/7.3/'},
-                    {text: '7.4', link: '/7.4/'},
-                ],
-            },
 
             {
                 text: 'PHP 8',
                 activeMatch: '/8',
                 items: [
-                    {text: '8.0', link: '/8.0/'},
-                    {text: '8.1', link: '/8.1/'},
-                    {text: '8.2', link: '/8.2/'},
-                    {text: '8.3', link: '/8.3/'},
                     {text: '8.4', link: '/8.4/'},
+                    {text: '8.3', link: '/8.3/'},
+                    {text: '8.2', link: '/8.2/'},
+                    {text: '8.1', link: '/8.1/'},
+                    {text: '8.0', link: '/8.0/'},
+                ],
+            },
+
+            {
+                text: 'PHP 7',
+                activeMatch: '/7',
+                items: [
+                    {text: '7.4', link: '/7.4/'},
+                    {text: '7.3', link: '/7.3/'},
+                    {text: '7.2', link: '/7.2/'},
+                    {text: '7.1', link: '/7.1/'},
+                    {text: '7.0', link: '/7.0/'},
                 ],
             },
         ],
